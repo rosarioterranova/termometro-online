0e2dae9d1d73a99dcb755bba816bfcb5b35d4812
Rosario Terranova <rosarioterranova91@gmail.com>
2020-06-22 11:41:19 +0200

    added config

---
+++ public/app.config.js
@@ -0,0 +1,16 @@
+termometro.config(function($locationProvider, $routeProvider){
+    $locationProvider.hashPrefix(''); //remove the hash-bang
+    $routeProvider
+
+    .when("/", {
+        templateUrl: "pages/main.html"
+    })
+
+    .when("/about", {
+        templateUrl: "pages/about.html"
+    })
+
+    .when("/help", {
+        templateUrl: "pages/help.html"
+    })
+})
--- public/app.module.js
+++ public/app.module.js
@@ -1,18 +1,5 @@
-const termometro = angular.module('termometro', ["ngRoute", "navBar", "temperatureQuery"]) 
-
-termometro.config(function($locationProvider, $routeProvider){
-    $locationProvider.hashPrefix(''); //remove the hash-bang
-    $routeProvider
-
-    .when("/", {
-        templateUrl: "pages/main.html"
-    })
-
-    .when("/about", {
-        templateUrl: "pages/about.html"
-    })
-
-    .when("/help", {
-        templateUrl: "pages/help.html"
-    })
-})
+const termometro = angular.module('termometro', [
+    "ngRoute",
+    "navBar",
+    "temperatureQuery"
+]) 
